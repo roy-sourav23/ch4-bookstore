8ee2da4d7e64950d80952af19482ad7b55b05407
roy-sourav23 <rsourav2023@gmail.com>
2023-03-30 21:25:35 +0530

    build.sh

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".herokuapp"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".onrender.com"]
 
 # Application definition
 
